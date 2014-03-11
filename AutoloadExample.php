954242c8ce8c449a7ec59da5e005cf97ad724063
parminder <perminder.klair@gmail.com>
2014-03-11 22:34:27 +0000

    first push

---
+++ AutoloadExample.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace kato;
+
+/**
+ * Usage: \kato\DropZone::widget();
+ * Class DropZone
+ * @package kato
+ */
+class DropZone extends \yii\base\widget
+{
+    function run()
+    {
+        return "Hello!";
+    }
+}
